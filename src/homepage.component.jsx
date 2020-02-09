ee5c73111a467d06618e3db36335848616d0d4ae
unknown <kelvin.nyadzayo16@gmail.com>
2020-02-09 20:55:39 +0200

    added homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='HomePage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <h1 className='subtitle'>SHOP NOW</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JACKETS</h1>
+                    <h1 className='subtitle'>SHOP NOW</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <h1 className='subtitle'>SHOP NOW</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SNEAKERS</h1>
+                    <h1 className='subtitle'>SHOP NOW</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMANS</h1>
+                    <h1 className='subtitle'>SHOP NOW</h1>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>MANS</h1>
+                        <h1 className='subtitle'>SHOP NOW</h1>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+)
+export default HomePage;
+
